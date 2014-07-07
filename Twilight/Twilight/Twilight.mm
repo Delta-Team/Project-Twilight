afd8f86882d679c3ff029e0e5a4c947191540c12
Meirtz <593865730@qq.com>
2014-07-07 13:08:55 +0800

    d
    ok

--- Twilight/Twilight/Twilight.mm
+++ Twilight/Twilight/Twilight.mm
@@ -1,33 +1,22 @@
-<<<<<<< HEAD
-#line 1 "/Users/meilingrui/Desktop/Github_GLProjects/Delta-Team/Project Twilight/Twilight/Twilight/Twilight.xm"
-=======
 #line 1 "/Users/BlueCocoa/Desktop/My Projects/Project-Twilight/Twilight/Twilight/Twilight.xm"
->>>>>>> FETCH_HEAD
 #import <UIKit/UIKit.h>
 
 
 @interface SBIconScrollView : UIView
 @end
 
-<<<<<<< HEAD
-=======
 @interface SpringBoard : UIApplication
 @end
 
 @interface SBAppWindow : UIWindow
 @end
 
->>>>>>> FETCH_HEAD
 #include <logos/logos.h>
 #include <substrate.h>
 @class SBIconScrollView; 
 static void (*_logos_orig$_ungrouped$SBIconScrollView$touchesMoved$withEvent$)(SBIconScrollView*, SEL, NSSet *, id); static void _logos_method$_ungrouped$SBIconScrollView$touchesMoved$withEvent$(SBIconScrollView*, SEL, NSSet *, id); 
 
-<<<<<<< HEAD
-#line 7 "/Users/meilingrui/Desktop/Github_GLProjects/Delta-Team/Project Twilight/Twilight/Twilight/Twilight.xm"
-=======
 #line 13 "/Users/BlueCocoa/Desktop/My Projects/Project-Twilight/Twilight/Twilight/Twilight.xm"
->>>>>>> FETCH_HEAD
 
 
 float last;
@@ -37,9 +26,6 @@ float last;
 static void _logos_method$_ungrouped$SBIconScrollView$touchesMoved$withEvent$(SBIconScrollView* self, SEL _cmd, NSSet * touches, id event) {
     UITouch *touch = [touches anyObject];
     CGPoint point = [touch locationInView:self];
-<<<<<<< HEAD
-    if (last > point.y){
-=======
     if (last - point.y > 10.0f){
         
         SpringBoard *sb = (SpringBoard *)[UIApplication sharedApplication];
@@ -65,7 +51,6 @@ static void _logos_method$_ungrouped$SBIconScrollView$touchesMoved$withEvent$(SB
         }];
         
         
->>>>>>> FETCH_HEAD
         
     }
     last = point.y;
@@ -76,8 +61,4 @@ static void _logos_method$_ungrouped$SBIconScrollView$touchesMoved$withEvent$(SB
 
 static __attribute__((constructor)) void _logosLocalInit() {
 {Class _logos_class$_ungrouped$SBIconScrollView = objc_getClass("SBIconScrollView"); MSHookMessageEx(_logos_class$_ungrouped$SBIconScrollView, @selector(touchesMoved:withEvent:), (IMP)&_logos_method$_ungrouped$SBIconScrollView$touchesMoved$withEvent$, (IMP*)&_logos_orig$_ungrouped$SBIconScrollView$touchesMoved$withEvent$);} }
-<<<<<<< HEAD
-#line 25 "/Users/meilingrui/Desktop/Github_GLProjects/Delta-Team/Project Twilight/Twilight/Twilight/Twilight.xm"
-=======
 #line 55 "/Users/BlueCocoa/Desktop/My Projects/Project-Twilight/Twilight/Twilight/Twilight.xm"
->>>>>>> FETCH_HEAD
